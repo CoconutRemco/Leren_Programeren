cf31a5b113d70bc8f0a8b46441dd7aba2731cd27
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-09-06 13:37:21 +0200

    input en if

---
+++ Meten is weten.py
@@ -0,0 +1,13 @@
+print("Vul een variabele in")
+a = int(input())
+
+print("Vul nog een variabele in")
+b = int(input())
+
+if a > b: 
+    print("a is het grootste getal " + str(a) )
+else:
+    print("a is helaas niet groter dan b")
+
+max = a
+
