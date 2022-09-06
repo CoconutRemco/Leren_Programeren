07c9eb17350d5dda0f1da7beeec027bbf7ccc39b
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-09-06 13:10:56 +0200

    pizzacalculator

---
+++ pizzacalculator.py
@@ -0,0 +1,60 @@
+print("Welkom bij pizzacalculator!")
+# Deze lijn geeft info
+print("Voer het aantal pizza's per groote in.")
+# Deze lijn geeft info
+print("Hoeveel pizza's small wilt u?")
+# Deze lijn stelt een vraag
+small = int(input())
+# Deze lijn vraagt om een waarde
+
+print("Hoeveel pizza's medium wilt u?")
+# Deze lijn stelt een vraag
+medium = int(input())
+# Deze lijn vraagt om een waarde
+
+print("Hoeveel pizza's large wilt u?")
+# Deze lijn stelt een vraag
+large = int(input())
+# Deze lijn vraagt om een waarde
+sprice = 3
+mprice = 6
+lprice = 10
+
+print("Totaal prijs pizza's small")
+# Deze lijn geeft info
+print(small * sprice)
+# Deze lijn geeft een waarde
+
+print("Totaal prijs pizza's medium")
+# Deze lijn geeft info
+print(medium * mprice)
+# Deze lijn geeft een waarde
+
+print("Totaal prijs pizza's large")
+# Deze lijn geeft info
+print(large * lprice)
+# Deze lijn geeft een waarde
+
+print("Totaal Bon:")
+# Deze lijn geeft info
+
+print("------------------------------")
+
+print(small * sprice)
+# Deze lijn geeft een waarde
+
+print(medium * mprice)
+# Deze lijn geeft een waarde
+
+print(large * lprice)
+# Deze lijn geeft een waarde
+
+print("Totaal Bedrag:")
+# Deze lijn geeft info
+
+print(small * sprice + medium * mprice + large * lprice)
+# Deze lijn geeft een waarde
+
+
+print("------------------------------")
+
