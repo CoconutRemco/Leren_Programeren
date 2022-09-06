240fa13a8569602ae229f5bb8be1e3cd2a60cf50
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-09-06 10:07:50 +0200

    Speelhal

---
+++ Speelhal.py
@@ -0,0 +1,12 @@
+a = 3
+b = 7.45
+c = 45*3/5*0.37
+d= 32.34
+
+print("Totaal bedrag EntreeTickets")
+print(a * b)
+print("Totaal bedrag Vip-Vr Tickets")
+print(c)
+print("Totaal bedrag Entree + Vip-Vr Tickets")
+print(d)
+
