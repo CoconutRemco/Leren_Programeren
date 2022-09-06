c8cf43e39366355d25f35083bee8ffa3e845dd49
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-09-06 11:01:31 +0200

    Updates

--- Feestlunch.py
+++ Feestlunch.py
@@ -6,14 +6,24 @@ e = 3*0.50
 totaal_bedrag_is = 12.19-1.50
 
 print("Croissantjes Zijn")
+# Deze regel geeft informatie
 print(a * b)
+# Deze regel geeft een waarde
 print("Stokbroden Zijn")
+# Deze regel geeft informatie
 print(c * d)
+# Deze regel geeft een waarde
 print("Bedrag Zonder Korting Is")
+# Deze regel geeft informatie
 print(6.63 + 5.56)
+# Deze regel geeft een waarde
 print("Korting Is")
+# Deze regel geeft informatie
 print(e)
+# Deze regel geeft een waarde
 print("Totaal Bedrag Is")
+# Deze regel geeft informatie
 print(totaal_bedrag_is)
+# Deze regel geeft een waarde
 
 print("De feestlunch kost je bij de bakker  " +str(totaal_bedrag_is)+ "  Dit is erg voordelig")
--- Speelhal.py
+++ Speelhal.py
@@ -5,10 +5,16 @@ d= 52.12
 
 
 print("Totaal bedrag EntreeTickets")
+# Deze regel geeft informatie
 print(a * b)
+# Deze regel geeft informatie
 print("Totaal bedrag Vip-Vr Tickets")
+# Deze regel geeft informatie
 print(c)
+# Deze regel geeft informatie
 print("Totaal bedrag Entree + Vip-Vr Tickets")
+# Deze regel geeft informatie
 print(d)
-
+# Deze regel geeft een waarde
 print("De hele dag in de speelhal met 3 vrienden kost  " +str(d)+ "  dit is erg weinig")
+# Deze regel geeft informatie en een waarde
