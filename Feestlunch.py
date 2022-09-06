d2186788c48e615745027da55010e65596d4e039
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-09-06 09:58:39 +0200

    commit

---
+++ Feestlunch.py
@@ -0,0 +1,17 @@
+a = 0.39
+b = 17
+c = 2.78
+d = 2
+e = 3*0.50
+totaal_bedrag_is = 12.19-1.50
+
+print("Croissantjes Zijn")
+print(a * b)
+print("Stokbroden Zijn")
+print(c * d)
+print("Bedrag Zonder Korting Is")
+print(6.63 + 5.56)
+print("Korting Is")
+print(e)
+print("Totaal Bedrag Is")
+print(totaal_bedrag_is)
