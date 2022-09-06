26b2dda3147db6a4329130f331a22537ed46ee16
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-09-06 11:16:38 +0200

    person

---
+++ person.py
@@ -0,0 +1,13 @@
+print("Wat is uw naam?")
+naam = input()
+print("Hallo " + naam + " Wat is uw Woonplaats?" )
+woonplaats = input()
+print("Bedankt, Wat is uw adres?")
+adres = input()
+print("Bedankt, Wat is uw postcode?")
+postcode = input()
+
+print("Naam: " + naam)
+print("Adres " + adres)
+print("Postcode: " + postcode)
+print("Woonplaats " + woonplaats)
