72fb5f6558c81a4991b6de7c03550bad1b9983c4
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-09-07 11:38:55 +0200

    updated

--- Sollicitatie.py
+++ Sollicitatie.py
@@ -2,58 +2,60 @@ print("Ja is 1 nee is 2")
 nee = 2
 print("Hoeveel jaar ervaring heeft u in de dierendressuur of jongleren of acrobatiek?")
 vraag1 = input()
-if int(vraag1) > int(3):
-    print("Bent u in het bezit van een mbo-4 ondernemen diploma?")
-else:
-    print("Helaas u valt af")
+
+print("Bent u in het bezit van een mbo-4 ondernemen diploma?")
+
 vraag2 = input()
-if nee > int(vraag2):
-    print("Bent u in bezit van een geldig vrachtwagen rijbewijs?")
-else:
-    print("Helaas valt u af.")
+
+print("Bent u in bezit van een geldig vrachtwagen rijbewijs?")
+
 vraag3 = input()
-if nee > int(vraag3):
-    print("Bent u in bezit van een hoge hoed?")
-else:
-    print("Helaas valt u af.")
+print("Bent u in bezit van een hoge hoed?")
+
 vraag4 = input()
-if nee > int(vraag4):
-    print("Bent u een man met snor langer dan 10cm of vrouw met rood krulhaar van langer dan 20 cm?")
+
+print("Bent u een man of een vrouw? 1 man 2 vrouw")
+manofvrouw = input()
+if int(manofvrouw) < 2:
+    print("Heeft u een snor van 10cm of langer?")
+    snorlengte = input()
 else:
-    print("Helaas valt u af.")
-vraag5 = input()
-if nee > int(vraag5):
-    print("Hoe lang bent u?")
-else:
-    print("Helaas valt u af.")
+    print("Heeft u rood gekrult haar van 20cm of langer?")
+    roodhaar = input()
+
+
+
+print("Hoe lang bent u?")
+
+    
 vraag6 = input()
-if int(150) > int(vraag6):
-    print("Helaas valt u af.")
-else:
-    print("Hoeveel weegt u?")
+
+    
+
+print("Hoeveel weegt u?")
 vraag7 = input()
-if int(90) > int(vraag7):
-    print("Helaas u valt af.")
-else:
-    print("Heeft u een certificaat overleven met gevaarlijk personeel?")
+
+    
+
+print("Heeft u een certificaat overleven met gevaarlijk personeel?")
 vraag8 = input()
-if nee > int(vraag8):
-    print("Bent u capabel?")
-else:
-    print("U heeft het helaas niet behaald.")
+
+print("Bent u capabel?")
+
+    
 vraag9 = input()
-if nee > int(vraag9):
-    print("Rijd u vaak onder invloed?")
-else:
-    print("Helaas u heeft het niet gehaald.")
+
+print("Rijd u vaak onder invloed?")
+
+    
 vraag10 = input()
-if nee > int(vraag10):
-    print("Bent u op dit moment onder invloed?")
-else:
-    print("Helaas u heeft het niet gehaald.")
+
+print("Bent u op dit moment onder invloed?")
+
 vraag11 = input()
-if nee > int(vraag11):
-    print("U heeft de test behaald gefeliciteerd!")
+
+if int(vraag1) > 3 and int(vraag2) < 2 and int(vraag3) < 2 and int(vraag4) < 2  and int(vraag6) > 150 and int(vraag7) > 90 and int(vraag8) < 2 and int(vraag9) <2 and int(vraag10) <2 and int(vraag11) <2 and int(snorlengte) > 10 and int(roodhaar) > 20:
+    print("U bent aangenomen!")
 else:
-    print("Helaas u heeft door de laatse vraag de test niet behaald.")
+    print("Helaas u heeft het niet gehaald!")
 
