7f2f79283319a9bf3688f8fe8a0f3bac5ad02bf9
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-09-12 10:15:17 +0200

    Pork.py

---
+++ pork.py
@@ -0,0 +1,59 @@
+print("Official Copyrighted by PorkEnterprises. Dont you dare to copy!")
+
+print("This is an story based game about pigs")
+print("Press enter to go to next sentence!")
+
+input("There is a crowd of pigs on the other side of the ocean!")
+input("Do you want to go there by boat or by helicopter?")
+input("Press 1 for boat Press 2 for helicopter")
+
+Task1 = input()
+if Task1 == '1' :
+    input("Oke cool now get to the shore to find wood and build an boat!")
+    input("Oke we are at the shore now and we are lucky there is already an boat there!")
+    input("Good job finding the boat we made it to the shore undamaged")
+    input("Now whe are here and we see the pigs what do you wanna do?")
+    input("Press 1 to slaughter the pigs and eat them tonight. Press 2 to save them and bread more.")
+    Task3 = input()
+    if Task3 == '1':
+        input("Well hey boy now you have to slaughter them 1 by 1 without getting them mad!")
+        input("Well hey you fucked up they are mad as fuck now you have to escape!")
+        input("Press 1 to run Press 2 to steal the scooter at the waterside")
+        Task4 = input()
+        if Task4 == "1":
+            input("Fuck wrong decission now you are dead")
+            input("Game over")
+        else:
+            input("Shit dog there's no fuel in it you're dead.")
+            input("Game over")
+            
+    else:
+        input("Hey good choise go get some carrots to breed!")
+        input("Do you wanna buy carrots at the farm or get them for free at the river?")
+        input("Press 1 to buy Press 2 to get for free")
+        Task5 = input()
+        if Task5 == '1':
+            input("Well good choise these are way better qaulity")
+            input("You did an really good job now you breeded them and they are so big already!")
+            input("You have reached the end of the game well done!")
+        else:
+            input("These carrots are so bad qaulity that you can't use them you'r yourney is over!")
+            input("Game over")
+    
+elif Task1 == '2' :
+    input("Wow nice choice now we have to steal one from uncle Brooke!")
+    input("Oke we just arrived at uncle Brooke he seems to have a couple of helicopters we only need a 2 seater!")
+    input("Press 1 to steal press 2 to pay rent!")
+    Task2 = input()
+    if Task2 == '1':
+        input("Oh shit boy hes hella mad we have to make it up to him later!")
+    else:
+        input("Its gonna cost some money but atleast he isn't mad!")
+    input("Ooh fuck man you crashed the helicopter in the water we will drown now")
+    input("Game over do it better next time!")
+else:
+    input("Hey you didn't anwser 1 or 2 try again!")
+
+
+    
+
